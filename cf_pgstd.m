5a92a7f120fcffad19d050e5d6d796c4a25b00a1
Duong Hai Long <dhlong.hp@gmail.com>
2015-12-01 01:11:52 +0800

    combine all the cf_ files into one

--- cf_pgstd.m
+++ cf_pgstd.m
@@ -1,4 +1,4 @@
-function cf_pgstd(trance)
+function cf_pgstd(trance, cf_type)
 
 %CF_PGSTD Summary of this function goes here
 %   Detailed explanation goes here
@@ -6,7 +6,6 @@ function cf_pgstd(trance)
 if ~ischar(trance); trance = num2str(trance); end
 
 settings = loadSettings;
-datafile = ['trance-' trance '-' settings.result_file];
 newpg = settings.newpg;
 carstd = settings.carstd;
 truckstd = settings.truckstd;
@@ -15,31 +14,61 @@ if ischar(newpg); newpg = str2double(newpg); end;
 if ischar(carstd); carstd = str2double(carstd); end;
 if ischar(truckstd); truckstd = str2double(truckstd); end;
 
+datafile = ['trance-' trance '-' settings.result_file];
 load(datafile);
 
-diaryname = ['diary-cf-pgstd-trance-' trance '-run-' runid '.txt'];
+diaryname = ['diary-cf-' cf_type '-trance-' trance '-run-' runid '.txt'];
 
-Data.pgreal = newpg;
-madpm = 1./mampg*100*newpg;
+if ~isempty(strfind(cf_type, 'pg'))
+    Data.pgreal = newpg;
+    madpm = 1./mampg*100*newpg;
 
-Xrc = Data.Xrc;
-Xv = Data.Xv;
-Xrc(:, madpm_idx_rc) = madpm./Data.income09;
-Xv(:, madpm_idx_v) = madpm./Data.income09;
-Data.Xv = Xv;
-Data.Xrc = Xrc;
-Data.XrcV = bsxfun(@times, Data.Xrc, Data.v);
+    Xrc = Data.Xrc;
+    Xv = Data.Xv;
+    Xrc(:, madpm_idx_rc) = madpm./Data.income09;
+    Xv(:, madpm_idx_v) = madpm./Data.income09;
+    Data.Xv = Xv;
+    Data.Xrc = Xrc;
+    Data.XrcV = bsxfun(@times, Data.Xrc, Data.v);
 
-deltas = bsxfun(@plus, Data.Xv*beta_v, xis);
+    deltas = bsxfun(@plus, Data.Xv*beta_v, xis);
+end
 
-Data.cafestd(car==1) = carstd;
-Data.cafestd(car==0) = truckstd;
-Data.cagpmstd = 1./Data.cafestd*100;
+if ~isempty(strfind(cf_type, 'std'))
+    Data.cafestd(car==1) = carstd;
+    Data.cafestd(car==0) = truckstd;
+    Data.cagpmstd = 1./Data.cafestd*100;
+end 
 
 coef = -eta(end-3:end);
 [gpm1, ps1, gammaj1, cce1, share1] = contraction_tech(theta, deltas(:,1:1), cs(:,1:1), Data, cce, cce, coef, ps, gammaj0, diaryname);
 
-resultfile = ['cf-pgstd-trance-' trance '-run-' runid '.mat'];
+
+pow = numel(coef)-1;
+f = @(x) bsxfun(@power, x, 0:pow)*coef;
+int_f = @(x) bsxfun(@power, x, 1:pow+1)*(coef./(1:pow+1)');
+f0 = f(cce);
+int_f0 = int_f(cce);
+
+Data.gpm = gpm1;
+Data.dpm = gpm1.*Data.pgreal;
+delta1 = bsxfun(@plus, Data.Xv*beta_v, xi);
+change_c = (f(cce1).*cce1 - f0.*cce - int_f(cce1) + int_f0)*10;
+change_c(isnan(change_c)) = 0;
+c1 = bsxfun(@plus, c, change_c);
+
+[p2, margin2, s2, gammaj2] = contraction_cafe(theta, delta1, c1, Data, gammaj1, ps1, settings);
+share2 = mean(s2,2);
+
+index = (car == 1);
+carcafe = sum(share2(index))/sum(share2(index).*gpm1(index));
+carmpg = mean(1./gpm1(index)*100);
+
+index = (car == 0);
+truckcafe = sum(share2(index))/sum(share2(index).*gpm1(index));
+truckmpg = mean(1./gpm1(index)*100);
+
+resultfile = ['cf-' cf_type '-trance-' trance '-run-' runid '.mat'];
 save(resultfile);
 
 diary off;
